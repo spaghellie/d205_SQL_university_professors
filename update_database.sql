5913f61acf50542a6f581ed59071a4989f1e56f9
Ellie Weeks <89754113+spaghellie@users.noreply.github.com>
2024-10-27 14:40:23 -0600

    Update update_database.sql

--- update_database.sql
+++ update_database.sql
@@ -24,5 +24,47 @@ CREATE TABLE universities (
 ALTER TABLE professors
 ADD COLUMN university_shortname text;
 
--- We now need to migrate the original data to these new tables
+-- -- -- -- -- -- -- -- -- --
 
+-- We now need to migrate the original data to these new tables
+-- -- -- -- --
+-- Insert unique professors into the new table
+INSERT INTO professors 
+SELECT DISTINCT firstname, lastname, university_shortname 
+FROM university_professors;
+
+-- Insert unique universities into the new table
+
+-- Insert unique affiliations into the new table
+INSERT INTO affiliations 
+SELECT DISTINCT firstname, lastname, function, organization 
+FROM university_professors;
+
+-- Insert unique organizations into the new table
+
+-- -- -- -- -- -- -- -- -- --
+
+-- We must now delete the unnecessary university_professors table (because we no longer need it)
+-- -- -- -- --
+-- Delete the university_professors table
+DROP TABLE university_professors;
+
+-- -- -- -- -- -- -- -- -- --
+
+-- We can set up some constraints
+-- -- -- -- --
+-- Disallow NULL values in firstname
+ALTER TABLE professors 
+ALTER COLUMN firstname SET NOT NULL;
+
+-- Disallow NULL values in lastname
+ALTER TABLE professors
+ALTER COLUMN lastname SET NOT NULL;
+
+-- Make universities.university_shortname unique
+ALTER TABLE universities
+ADD CONSTRAINT university_shortname_unq UNIQUE(university_shortname);
+
+-- Make organizations.organization unique
+ALTER TABLE organizations
+ADD CONSTRAINT organization_unq UNIQUE(organization);
