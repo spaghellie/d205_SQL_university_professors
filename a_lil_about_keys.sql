a307ccca0ce56511a3913961b95333de971dc3a6
Ellie Weeks <89754113+spaghellie@users.noreply.github.com>
2024-10-28 10:26:21 -0600

    Create a_lil_about_keys.sql

---
+++ a_lil_about_keys.sql
@@ -0,0 +1,19 @@
+-- Here is a quick review exercise from DataCamp on attributes & key constraints.
+
+-- Pretend there is some entity type, "student."
+-- A student has:
+--    - a last name consisting of up to 128 characters (required),
+--    - a unique social security number, consisting only of integers, that should serve as a key,
+--    - a phone number of fixed length 12, consisting of numbers and characters (but some students don't have one).
+-- Given the above description of a student entity, create a table students with the correct column types.
+
+-- -- -- SOLUTION: -- -- --
+
+-- Create the table
+CREATE TABLE students (
+  last_name varchar(128) NOT NULL,
+  ssn int PRIMARY KEY,
+  phone_no char(12)
+);
+-- NOTE that we added a PRIMARY KEY for the ssn;
+-- and that there is no formal length requirement for the integer column. The application would have to make sure it's a correct SSN!
